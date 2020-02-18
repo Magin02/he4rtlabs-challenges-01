be57ae73d12ac938a1e8d1917a813463ccdbab12
leal <gabrielleao.mb@gmail.com>
2020-02-18 19:24:14 -0300

    Realização do projeto

---
+++ src/js/main.js
@@ -0,0 +1,36 @@
+btnCalculo = document.querySelector("button");
+
+btnCalculo.onclick = function () {
+    var inputsElements = document.querySelectorAll("input");
+
+    var horasDiarias = parseFloat(inputsElements[0].value);
+    var diasEfetivos = parseInt(inputsElements[1].value);
+    var diasFerias = parseInt(inputsElements[2].value);
+    var valorProjeto = parseFloat(inputsElements[3].value);
+
+    var valorHora = (valorProjeto / (diasEfetivos * 4 * horasDiarias)) + ((diasFerias * diasEfetivos * horasDiarias))
+    var elementResult = document.querySelector("h1#result");
+
+    if (elementResult.firstChild !== null) {
+        elementResult.removeChild(elementResult.firstChild);
+    }
+    elementResult.appendChild(document.createTextNode("A hora vale: R$" + valorHora ));
+    elementResult.classList.add("animated", "animatedFadeInUp", "fadeInUp");
+
+    window.scrollTo(0, 1000);
+};
+
+
+function onlynumber(evt) {
+    var theEvent = evt || window.event;
+    var key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode( key );
+    //var regex = /^[0-9.,]+$/;
+    var regex = /^[0-9.]+$/;
+    if( !regex.test(key) ) {
+       theEvent.returnValue = false;
+       if(theEvent.preventDefault) theEvent.preventDefault();
+    }
+ }
+
+
